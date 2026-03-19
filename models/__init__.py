d04959781f3a4db12acf635739720081ba1ebdac
holyfudgencannoli <moshimoshi8246@gmail.com>
2026-03-19 17:38:20 -0400

    "v0.3.0 feature: Event repo and model"

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,11 @@
 
 from db import Base, engine
-<<<<<<< HEAD
 from .user import User
 from .authenticator import Authenticator
+from .event import Event
 
 __all__ = [
     "User",
-    "Authenticator"
-=======
-
-from .user import User
-
-__all__ = [
-    "User"
->>>>>>> aa05fc8bb9a1d60f24160b55a4e2fa93ffb8bfd6
+    "Authenticator",
+    "Event"
 ]
